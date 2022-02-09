85a7295d7a04048fc8dd29aea8944db5e41abde6
ahDevGit <justalihaider@gmail.com>
2022-02-10 02:25:58 +0600

    login Validation

---
+++ js/bank.js
@@ -0,0 +1,17 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+  //Get User Email
+  const emailField = document.getElementById('user-email');
+  const userEmail = emailField.value;
+
+  //Get User Password
+  const passwordField = document.getElementById('user-password');
+  const userPassword = passwordField.value;
+
+  if (userEmail == 'ali@gmail.com' && userPassword == 'secret') {
+    console.log('Valid User');
+    window.location.href = 'banking.html';
+  }
+  else {
+    console.log('Invalid User');
+  }
+})
